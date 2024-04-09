caa4b320678b4324e0c8aaa7a0a09f28a2290700
dbhatt <dbhatt@journaltech.com>
2024-04-09 09:25:49 -0700

    test security

---
+++ src/main/java/com/shipmentEvents/handlers/Main.java
@@ -0,0 +1,15 @@
+package com.shipmentEvents.handlers;
+
+// Define a public class named Main. The class name should match the filename.
+public class Main {
+
+    // The main method, serving as the entry point for the program.
+    public static void main(String[] args) {
+
+        // Print "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+
+        // Add your code here.
+
+    }
+}
