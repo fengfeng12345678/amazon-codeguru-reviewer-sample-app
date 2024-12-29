ffc15cc61152b98354260bee6a171ad6d0451e1f
zhaoyifeng <zhaoyifeng@lixiang.com>
2024-12-29 22:09:33 +0800

    feat: add math utils

---
+++ src/main/java/com/math/MathUtils.java
@@ -0,0 +1,11 @@
+package com.math;
+
+public class MathUtils {
+    public int plus(int a, int b) {
+        return a + b;
+    }
+
+    public int mimus(int a, int b) {
+        return a - b;
+    }
+}
